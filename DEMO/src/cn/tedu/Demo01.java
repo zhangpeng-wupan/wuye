ea8fc1227eaddc38bf1a9e8f97b7fa5a7aa0f1b7
zhangpeng-wupan <1091335980@qq.com>
2019-07-16 15:55:38 +0800

    尝试第一次往github上上传代码

---
+++ DEMO/src/cn/tedu/Demo01.java
@@ -0,0 +1,19 @@
+package cn.tedu;
+
+import java.util.Arrays;
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+//		String str = "aa";
+//		byte[] by = str.getBytes();
+//		System.out.println(Arrays.toString(by));
+//		char s = 97;
+//		System.err.println(s);
+//		int a = 'a';
+//		int num = 'A';
+//		System.out.println(num);
+	
+		System.err.println(2<<3);
+	}
+}
--- hello-world/README.md
+++
@@ -1,4 +0,0 @@
-# hello-world
-你好，世界
-这是什么啊
-lunix hello
